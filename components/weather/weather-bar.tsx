1e4c2edef2b4a43c32e3bbad78c5d179f9501722
InfiniteScope <Kzxia001@gmail.com>
2026-08-25 15:11:29 +0800

    fix: move weather bar below navbar to prevent overlap

--- components/weather/weather-bar.tsx
+++ components/weather/weather-bar.tsx
@@ -2,7 +2,7 @@ import { WeatherWidget } from "@/components/weather/weather-widget"
 
 export function WeatherBar() {
   return (
-    <div className="pointer-events-auto fixed left-1/2 top-7 z-50 hidden -translate-x-1/2 -translate-y-1/2 lg:block">
+    <div className="pointer-events-auto fixed left-1/2 top-[4.5rem] z-50 hidden -translate-x-1/2 lg:block">
       <WeatherWidget />
     </div>
   )
