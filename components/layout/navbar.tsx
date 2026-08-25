ab4e5711efc76f798242bbe460288b66139bf459
InfiniteScope <Kzxia001@gmail.com>
2026-08-25 15:19:27 +0800

    Revert "fix: integrate weather widget into navbar to prevent overlap"
    This reverts commit 0c0812d007cb37cbc4bda8c3780c1c4bcc95a791.

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -21,7 +21,6 @@ import { SearchCommand } from "@/components/search-command"
 import { FlowToggle } from "@/components/flow/flow-toggle"
 import { AppearanceToggle } from "@/components/theme/appearance-toggle"
 import { MusicPlayerMini } from "@/components/music/music-player-mini"
-import { WeatherWidget } from "@/components/weather/weather-widget"
 import type { Danmaku } from "@prisma/client"
 import type { Post } from "@/lib/mdx"
 
@@ -128,11 +127,6 @@ export function Navbar({ danmaku, posts, unreadCount = 0 }: NavbarProps) {
               )
             })}
           </nav>
-        </div>
-
-        {/* Center: Weather Widget */}
-        <div className="hidden flex-1 justify-center xl:flex">
-          <WeatherWidget />
         </div>
 
         {/* Right Actions */}
--- components/layout/shell.tsx
+++ components/layout/shell.tsx
@@ -9,6 +9,7 @@ import { PageTransition } from "@/components/motion/page-transition"
 import { MusicProvider } from "@/components/music/music-provider"
 import { MusicCollapseController } from "@/components/music/music-collapse-controller"
 import { MusicPlayerOverlay } from "@/components/music/music-player-overlay"
+import { WeatherBar } from "@/components/weather/weather-bar"
 import { FlowProvider } from "@/components/flow/flow-provider"
 
 interface ShellProps {
@@ -27,6 +28,7 @@ export async function Shell({ children }: ShellProps) {
       <SidebarCollapseProvider sidebar={<Sidebar danmaku={danmaku} />}>
         <FlowProvider>
           <MusicCollapseController />
+          <WeatherBar />
           <Navbar danmaku={danmaku} posts={posts} unreadCount={unreadCount} />
           <MusicPlayerOverlay />
           <main className="flex-1 px-4 py-6 md:px-6 lg:px-8">
